a17bc4bf2be411860408a3a7d9e093890e6f6ac0
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2020-11-15 22:00:28 +0100

    created new vue project

--- ClientApp/babel.config.js
+++ ClientApp/babel.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  "presets": [
-    [
-      "@vue/cli-plugin-babel/preset",
-      {
-        "useBuiltIns": "entry"
-      }
-    ]
+  presets: [
+    '@vue/cli-plugin-babel/preset',
   ],
-  "plugins": [
-    [
-      "transform-imports",
-      {
-        "vuetify": {
-          "transform": "vuetify/es5/components/${member}",
-          "preventFullImport": true
-        }
-      }
-    ]
-  ]
-}
+};
