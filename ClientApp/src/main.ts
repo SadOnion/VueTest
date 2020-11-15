78c6bc43d4a487217c8e592db80c94686636c3e7
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2020-11-15 22:28:12 +0100

    should log store to the console after mounted

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,12 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueNativeSock from 'vue-native-websocket'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(VueNativeSock, 'ws://localhost:9090', { store })
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+	router,
+	store,
+	render: h => h(App),
+}).$mount('#app')
--- ClientApp/src/views/Home.vue
+++ ClientApp/src/views/Home.vue
@@ -1,16 +1,15 @@
 <template>
-	<div class="home">yoo</div>
+	<div class="home">hello</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import VueNativeSock from 'vue-native-websocket'
 import store from '@/store'
 
 export default Vue.extend({
 	name: 'Home',
-	created() {
-		Vue.use(VueNativeSock, 'ws://localhost:9090', { store })
+	mounted() {
+		console.log(store)
 	},
 })
 </script>
