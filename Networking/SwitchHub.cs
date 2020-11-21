a0e3369d25a95c1cdf32f04a76550665008bd7c6
Sad Onion <cebulasmutna@gmail.com>
2020-11-21 19:27:36 +0100

    Create SwitchHub.cs

---
+++ Networking/SwitchHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Networking
+{
+    public class SwitchHub : Hub
+    {
+        private bool GlobalState = false;
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("SwitchStateChanged",GlobalState);
+        }
+        public async Task ChangeState(bool newState)
+        {
+            GlobalState = newState;
+            await Clients.All.SendAsync("SwitchStateChanged",GlobalState);
+        }
+    }
+}
