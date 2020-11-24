6a2f6e89a9fbae528542e8a585988f3dc9022dc9
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2020-11-24 21:30:59 +0100

    Reroll button

--- ClientApp/src/views/LabyrinthGenerator.vue
+++ ClientApp/src/views/LabyrinthGenerator.vue
@@ -34,16 +34,25 @@
 					</div>
 				</div>
 			</div>
-			<div class="resize" @mousedown.prevent="resizeDrag">
-				<svg
-					id="icon"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 32 32"
-				>
-					<polygon
-						points="25 11 23.59 12.41 26.17 15 17 15 17 5.83 19.59 8.41 21 7 16 2 11 7 12.41 8.41 15 5.83 15 15 5.83 15 8.41 12.41 7 11 2 16 7 21 8.41 19.59 5.83 17 15 17 15 26.17 12.41 23.59 11 25 16 30 21 25 19.59 23.59 17 26.17 17 17 26.17 17 23.59 19.59 25 21 30 16 25 11"
-					/>
-				</svg>
+			<div class="buttons-frame">
+				<button class="reroll" @click="reroll">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+						<path
+							d="M25.95,7.65l.0047-.0039c-.0918-.1094-.197-.2061-.2925-.3125-.1841-.2051-.3672-.41-.5635-.603-.1382-.1358-.2856-.2613-.43-.3907-.1831-.1645-.3657-.3286-.5581-.4824-.1592-.1279-.3244-.2466-.4895-.3667-.1921-.14-.3855-.2768-.5854-.4062-.1743-.1128-.3523-.2188-.5322-.3238q-.3081-.1786-.6253-.3408c-.1846-.0942-.37-.1846-.56-.27-.2224-.1-.449-.1914-.678-.2793-.1894-.0723-.3777-.1455-.5713-.209-.2463-.0815-.498-.1494-.7507-.2163-.1848-.0493-.3674-.1025-.5554-.1431-.29-.0634-.5865-.1074-.8833-.1508-.159-.023-.3145-.0552-.4754-.0728A12.9331,12.9331,0,0,0,6,7.7031V4H4v8h8V10H6.8115A10.961,10.961,0,0,1,16,5a11.1114,11.1114,0,0,1,1.189.0669c.1362.0146.268.042.4026.0615.2509.0366.5014.0742.7468.1275.1592.0346.3144.08.4712.1215.2131.0562.4258.1138.6335.1822.1643.0547.325.1167.4859.1782.1926.0742.3835.1509.5705.2349.1611.0727.3193.15.4763.23q.2677.1363.5262.2867c.153.0893.3046.18.4531.2758.1679.1089.3308.2242.4922.3413.1406.1026.2817.2037.417.3125.1616.1294.3156.2676.47.4063.1225.11.2478.2168.3652.332.1668.1636.3223.3379.4785.5117A10.9928,10.9928,0,1,1,5,16H3A13,13,0,1,0,25.95,7.65Z"
+						/>
+					</svg>
+				</button>
+				<div class="resize" @mousedown.prevent="resizeDrag">
+					<svg
+						id="icon"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 32 32"
+					>
+						<polygon
+							points="25 11 23.59 12.41 26.17 15 17 15 17 5.83 19.59 8.41 21 7 16 2 11 7 12.41 8.41 15 5.83 15 15 5.83 15 8.41 12.41 7 11 2 16 7 21 8.41 19.59 5.83 17 15 17 15 26.17 12.41 23.59 11 25 16 30 21 25 19.59 23.59 17 26.17 17 17 26.17 17 23.59 19.59 25 21 30 16 25 11"
+						/>
+					</svg>
+				</div>
 			</div>
 			<transition name="fade" :duration="200">
 				<div
@@ -132,6 +141,9 @@ export default Vue.extend({
 		}
 	},
 	methods: {
+		reroll() {
+			this.createMaze()
+		},
 		selectStart(x: number, y: number) {
 			this.selecting = true
 			this.selectSquare[0] = { x, y }
@@ -492,28 +504,69 @@ $wall-width: 1px;
 	}
 }
 
-.resize {
+.buttons-frame {
 	$size: $layout-03;
 	position: absolute;
-	width: $size;
-	height: $size;
-	left: calc(100% - #{$size}/ 2);
-	top: calc(100% - #{$size}/ 2);
-	background: $blue-60;
-	// border-radius: 50%;
-	cursor: grab;
+	right: -$size / 2;
+	bottom: -$size / 2;
 	display: flex;
 
-	svg {
-		margin: auto;
-		width: $layout-02;
-		fill: $white-0;
+	> * {
+		width: $size;
+		height: $size;
+		margin-left: $spacing-05;
+		background-color: $blue-60;
+		cursor: pointer;
+		outline: none;
+		border: none;
+
+		display: flex;
+
+		svg {
+			margin: auto;
+			width: $layout-02;
+			fill: $white-0;
+		}
+		transition: transform 0.2s ease-out, background-color 0.1s;
+		&:hover {
+			transition: transform 0.1s;
+			transform: scale(1.2);
+		}
+
+		&:active {
+			background-color: $blue-50;
+		}
 	}
 
-	transition: transform 0.2s ease-out;
-	&:hover {
-		transition: transform 0.1s;
-		transform: scale(1.2);
+	.reroll {
+		&:hover {
+			svg {
+				transition: transform 1s;
+				transform: rotateZ(-360deg);
+			}
+		}
+	}
+
+	.resize {
+		cursor: grab;
+		@keyframes show-moves {
+			from,
+			50%,
+			to {
+				transform: translateX(0);
+			}
+			25% {
+				transform: translateX(-4px);
+			}
+			75% {
+				transform: translateY(-4px);
+			}
+		}
+		&:hover {
+			svg {
+				animation: show-moves 1s 0.1s;
+			}
+		}
 	}
 }
 
